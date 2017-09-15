4b45383915afe219552ea4d2e3036debcd71811b
Roberts Gotlaufs <gotlaufs@gmail.com>
2017-09-15 23:36:05 +0300

    Add basic EEPROM reading logic

--- src/main.c
+++ src/main.c
@@ -1,42 +1,69 @@
-#include <wiringPiSPI.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <wiringPiSPI.h>
 #include "winbond_defines.h"
 #include "ports.h"
 #include "winbond_functions.h"
 
-int CHANNEL = 1;
-
 int main(){
-	int fd;
-	uint8_t sr1 = 0, sr2 = 0, sr3 = 0;		
-	spiInit(&fd);
+	int spi_dev;
+	uint8_t sr1 = 0, sr2 = 0, sr3 = 0;
+	uint8_t *eeprom_buffer;
+	FILE *eeprom_file, *sr_file;
+
+	printf("x250 BIOS chip utility\n\n")
 	
-	printf("Init result: %d\n",fd);
-	
-	printf("Attempt to read out status registers 1-3\n");
-	readSR(1, &sr1);
-	readSR(2, &sr2);
-	readSR(3, &sr3);	 
-	
-	printf("SR1 = %x\nSR2 = %x\nSR3 = %x\n", sr1, sr2, sr3);
-	
-	uint8_t temp = 0;
-	writeSR(1,  temp);
-	waitSPIAvailable();
-	readSR(1, &sr1);
-	readSR(2, &sr2);
-	readSR(3, &sr3);	 
-	
-	printf("SR1 = %x\nSR2 = %x\nSR3 = %x\n", sr1, sr2, sr3);
-	
-	temp = INS_WRITE_ENABLE;
-	spiWrite(&temp, 1);
-	readSR(1, &sr1);
-	readSR(2, &sr2);
-	readSR(3, &sr3);	 
-	
-	printf("SR1 = %x\nSR2 = %x\nSR3 = %x\n", sr1, sr2, sr3);
-	
+	printf("Opening SPI device..\n")
+	if (spiInit(&spi_dev)){
+		return 1;
+	}
+
+	printf("Reading status registers..\n")
+
+	if (readSR(1, &sr1)){
+		return 1;
+	}
+	if (ifreadSR(2, &sr2)){
+		return 1;
+	}
+	if (readSR(3, &sr3)){
+		return 1;
+	}
+	printf("SR1 = %x; SR2 = %x; SR3 = %x\n\n", sr1, sr2, sr3);
+
+	printf("Writing status register info to file: %s\n", REGISTER_FILE);
+	sr_file = fopen(REGISTER_FILE, "w");
+	if (sr_file = NULL){
+		return 1;
+	}
+
+	fprintf(sr_file, "Status registers (hex):\n");
+	fprintf(sr_file, "SR1 = %x\n", sr1);
+	fprintf(sr_file, "SR2 = %x\n", sr2);
+	fprintf(sr_file, "SR3 = %x\n", sr3);
+	fclose(sr_file);
+
+	printf("Reading entire BIOS chip into memory..\n");
+	buffer = (uint8_t *) malloc(MEMORY_SIZE);
+
+	if (readData(buffer, MEMORY_SIZE, 0x0)){
+		free(buffer);
+		return 1;
+	}
+
+	printf("Writing EEPROM dump to file: %s\n", EEPROM_FILE);
+	// Write in binary mode
+	eeprom_file = fopen(EEPROM_FILE, "wb");
+	if (eeprom_file = NULL){
+		free(buffer);
+		return 1;
+	}
+	fwrite(buffer, sizeof(uint8_t), sizeof(buffer), eeprom_file);
+	fclose(eeprom_file);
+
+	printf("All done!\n");
+	printf("Exiting..\n");
+	free(buffer);
 	return 0;
 }
