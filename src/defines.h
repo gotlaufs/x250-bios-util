c1176077c2b8ea4688f3d97549ae491ac5974c1d
Roberts Gotlaufs <gotlaufs@gmail.com>
2017-09-15 23:06:45 +0300

    Add file for global defines

---
+++ src/defines.h
@@ -0,0 +1,13 @@
+/*
+ * File: 	defines.h
+ *
+ * Various constants used in program
+ *
+ */
+#ifndef _DEFINES_H_
+#define _DEFINES_H_
+
+#define EEPROM_FILE		eeprom_dump
+#define REGISTER_FILE	status_registers.txt
+
+#endif // _DEFINES_H_
